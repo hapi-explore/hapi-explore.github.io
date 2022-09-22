fbee4289715ee7e4322f8368c0a99a56e3bf8de3
hapi-explore <593757535@qq.com>
2022-09-22 20:39:08 +0800

    update website

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.308c5fcd5680d598254b859246e6a3c4.js"
+  "./precache-manifest.95db3bb80f01276cacc7e37ac8f0905f.js"
 );
 
 self.addEventListener('message', (event) => {
